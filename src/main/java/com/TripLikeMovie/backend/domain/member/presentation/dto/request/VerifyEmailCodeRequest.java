52401692e8216b29784372314068fa7a21b01564
jongchan0109 <jongchan1009@naver.com>
2024-11-07 15:43:47 +0900

    feat: email, code 요청 DTO 구현 #3

---
+++ src/main/java/com/TripLikeMovie/backend/domain/member/presentation/dto/request/VerifyEmailCodeRequest.java
@@ -0,0 +1,15 @@
+package com.TripLikeMovie.backend.domain.member.presentation.dto.request;
+
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VerifyEmailCodeRequest {
+
+    @Email(message = "이메일 형식으로 입력해주세요.")
+    private String email;
+
+    private String code;
+}
