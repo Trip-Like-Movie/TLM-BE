169d2ce9d451794dffea56b3188ac23ce7d45372
jongchan0109 <jongchan1009@naver.com>
2024-11-21 21:12:01 +0900

    feat: SecurityUtils 구현

--- src/main/java/com/TripLikeMovie/backend/global/utils/security/SecurityUtils.java
+++ src/main/java/com/TripLikeMovie/backend/global/utils/security/SecurityUtils.java
@@ -1,15 +1,22 @@
 package com.TripLikeMovie.backend.global.utils.security;
 
 import com.TripLikeMovie.backend.global.error.exception.member.MemberNotFoundException;
+import com.TripLikeMovie.backend.global.security.auth.AuthDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+@Slf4j
 public class SecurityUtils {
     public static Integer getCurrentUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null) {
-            throw MemberNotFoundException.EXCEPTION;
+
+        if (authentication == null || !authentication.isAuthenticated() ||
+            authentication instanceof AnonymousAuthenticationToken) {
+            throw MemberNotFoundException.EXCEPTION; // 익명 유저일 경우 예외 던지기
         }
-        return Integer.valueOf(authentication.getName());
+        AuthDetails authDetails = (AuthDetails)authentication.getPrincipal();
+        return Integer.valueOf(authDetails.getUsername());
     }
 }
