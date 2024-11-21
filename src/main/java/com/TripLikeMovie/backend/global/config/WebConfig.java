b5dcaf1c72a15b811412bf1782e61dfdbbfb8052
jongchan0109 <jongchan1009@naver.com>
2024-11-21 21:12:14 +0900

    feat: 파일 조회 경로 구현

--- src/main/java/com/TripLikeMovie/backend/global/config/WebConfig.java
+++ src/main/java/com/TripLikeMovie/backend/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.TripLikeMovie.backend.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "PUT", "POST", "DELETE")
             .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 정적 리소스 매핑 설정
+        registry.addResourceHandler("/uploads/**")
+            .addResourceLocations("file:/Users/kimjongchan/TripLikeMovie/TLM-BE/uploads/"); // 실제 파일이 있는 경로
     }
 }
