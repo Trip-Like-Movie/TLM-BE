48eeef2e71fd37dd1c49ae90fc6e093c1f49a3e7
jongchan0109 <jongchan1009@naver.com>
2024-11-21 21:10:36 +0900

    feat: 사용자 정보 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/member/domain/vo/MemberInfoVo.java
@@ -0,0 +1,14 @@
+package com.TripLikeMovie.backend.domain.member.domain.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MemberInfoVo {
+
+    private final Integer id;
+    private final String email;
+    private final String nickname;
+    private final String profileImage;
+}
