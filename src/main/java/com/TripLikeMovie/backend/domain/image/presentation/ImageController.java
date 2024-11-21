cb360a0ddc172fdfafb5066068b9488d03178add
jongchan0109 <jongchan1009@naver.com>
2024-11-21 21:47:27 +0900

    feat: 프로필 사진 변경 기능 구현

--- src/main/java/com/TripLikeMovie/backend/domain/image/presentation/ImageController.java
+++ src/main/java/com/TripLikeMovie/backend/domain/image/presentation/ImageController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,11 @@ public class ImageController {
     @DeleteMapping("/profile")
     public void deleteProfileImage() {
         imageService.deleteProfileImage();
+    }
+
+    @PutMapping("/profile")
+    public void updateProfileImage(@RequestPart MultipartFile file) {
+        imageService.updateProfileImage(file);
     }
 
 }
