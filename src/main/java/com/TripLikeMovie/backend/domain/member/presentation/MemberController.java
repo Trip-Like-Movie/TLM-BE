2cf599c1e000ebb52dad39db69be9679e8c0e7b7
jongchan0109 <jongchan1009@naver.com>
2024-11-21 12:03:19 +0900

    fix: 응답 방식 변경

--- src/main/java/com/TripLikeMovie/backend/domain/member/presentation/MemberController.java
+++ src/main/java/com/TripLikeMovie/backend/domain/member/presentation/MemberController.java
@@ -11,10 +11,11 @@ import com.TripLikeMovie.backend.domain.member.service.email.EmailService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,45 +29,38 @@ public class MemberController {
     private final CredentialService credentialService;
 
     @PostMapping("/send-verification-code")
-    public ResponseEntity<Void> sendVerificationCode(
+    public void sendVerificationCode(
         @Valid @RequestBody SendVerificationRequest sendVerificationRequest) {
-        memberService.validateEmail(sendVerificationRequest); // 이메일이 중복되어 있는지 확인
 
+        log.info("verifying email : {}", sendVerificationRequest.getEmail());
+        memberService.validateEmail(sendVerificationRequest); // 이메일이 중복되어 있는지 확인
         emailService.sendVerificationCode(sendVerificationRequest); // 이메일에 인증코드 전송
-        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/verify-code")
-    public ResponseEntity<Void> verifyCode(
+    public void verifyCode(
         @Valid @RequestBody VerifyEmailCodeRequest verifyEmailCodeRequest) {
         emailService.verifyEmailCode(verifyEmailCodeRequest);
-        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/verify-nickname")
-    public ResponseEntity<Void> verifyNickname(
+    public void verifyNickname(
         @Valid @RequestBody VerifyNicknameRequest verifyNicknameRequest) {
+        log.info("Verifying nickname {}", verifyNicknameRequest.getNickname());
         memberService.validateNickname(verifyNicknameRequest);
-        return ResponseEntity.ok().build();
     }
 
-
     @PostMapping
-    public ResponseEntity<Void> createMember(
+    @ResponseStatus(HttpStatus.CREATED)
+    public AccessTokenAndRefreshTokenDto signUp(
         @Valid @RequestBody MemberSignUpRequest signUpRequest) {
 
         emailService.isEmailVerified(signUpRequest);
-
-        AccessTokenAndRefreshTokenDto accessTokenAndRefreshTokenDto = credentialService.signUp(
-            signUpRequest);
         emailService.deleteEmailVerificationCode(signUpRequest.getEmail());
 
         memberService.signUpSuccess(signUpRequest.getNickname());
 
-        return ResponseEntity.ok()
-            .header("access-token", "Bearer " + accessTokenAndRefreshTokenDto.getAccessToken())
-            .header("refresh-token", accessTokenAndRefreshTokenDto.getRefreshToken())
-            .build();
+        return credentialService.signUp(signUpRequest);
     }
 
 }
