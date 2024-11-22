3e3cff765e1b054887fd3989ce3498256038e13a
jongchan0109 <jongchan1009@naver.com>
2024-11-22 15:04:37 +0900

    feat: 영화 제목 검색 기능 구현

--- src/main/java/com/TripLikeMovie/backend/domain/movie/domain/repository/MovieRepository.java
+++ src/main/java/com/TripLikeMovie/backend/domain/movie/domain/repository/MovieRepository.java
@@ -1,9 +1,11 @@
 package com.TripLikeMovie.backend.domain.movie.domain.repository;
 
 import com.TripLikeMovie.backend.domain.movie.domain.Movie;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
     Optional<Movie> findByTitle(String title);
+    List<Movie> findByTitleContainingIgnoreCase(String title);
 }
