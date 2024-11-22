7a109fe4f1dbf15fc62ae873d70762a9a97478a1
jongchan0109 <jongchan1009@naver.com>
2024-11-22 10:58:07 +0900

    fix: 프로필 사진 제거

--- src/main/java/com/TripLikeMovie/backend/domain/image/domain/repository/ProfileImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.TripLikeMovie.backend.domain.image.domain.repository;
-
-import com.TripLikeMovie.backend.domain.image.domain.ProfileImage;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileImageRepository extends JpaRepository<ProfileImage, Integer> {
-    Optional<ProfileImage> findByMemberId(Integer memberId);
-}
