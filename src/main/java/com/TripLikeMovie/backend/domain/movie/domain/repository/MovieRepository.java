37f780ad90b36450c5cd159e107ef521ffa336b2
jongchan0109 <jongchan1009@naver.com>
2024-11-22 11:40:03 +0900

    feat: 영화 저장소 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/movie/domain/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.TripLikeMovie.backend.domain.movie.domain.repository;
+
+import com.TripLikeMovie.backend.domain.movie.domain.Movie;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    Optional<Movie> findByTitle(String title);
+}
