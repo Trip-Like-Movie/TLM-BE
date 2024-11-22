2437d5238a07908572e48fcefbcacc2ca6bc690e
jongchan0109 <jongchan1009@naver.com>
2024-11-22 11:39:32 +0900

    feat: 영화 생성 DTO 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/movie/presentation/dto/request/CreateMovieRequest.java
@@ -0,0 +1,10 @@
+package com.TripLikeMovie.backend.domain.movie.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateMovieRequest {
+    private String title;
+}
