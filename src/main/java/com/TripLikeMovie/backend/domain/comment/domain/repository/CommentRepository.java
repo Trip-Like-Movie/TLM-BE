0a5458120f76008400d7068930c20c3387143dd9
jongchan0109 <jongchan1009@naver.com>
2024-11-23 16:14:34 +0900

    feat: 댓글 레포지토리 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/comment/domain/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.TripLikeMovie.backend.domain.comment.domain.repository;
+
+import com.TripLikeMovie.backend.domain.comment.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+}
