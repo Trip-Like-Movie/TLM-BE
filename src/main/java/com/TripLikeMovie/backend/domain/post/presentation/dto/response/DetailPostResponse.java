23bbf5c515dd8a633b1e43da1e0d8c2b9bc8f04b
jongchan0109 <jongchan1009@naver.com>
2024-11-23 17:51:01 +0900

    feat: 게시글 상세 정보 반환 객체 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/post/presentation/dto/response/DetailPostResponse.java
@@ -0,0 +1,29 @@
+package com.TripLikeMovie.backend.domain.post.presentation.dto.response;
+
+import com.TripLikeMovie.backend.domain.comment.domain.vo.CommentVo;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class DetailPostResponse {
+
+    private Integer id;
+    private String content;
+    private List<String> imageUrls;
+    private String locationName;
+    private String locationAddress;
+    private List<CommentVo> comments;
+    private Integer likeCount;
+    private Boolean liked;
+
+    private Integer authorId;
+    private String authorNickname;
+    private String authorImageUrl;
+
+    private Integer movieId;
+    private String movieTitle;
+    private String movieImageUrl;
+
+}
