febf6b30d53a448fa7b18ee175d69da4d38276e2
jongchan0109 <jongchan1009@naver.com>
2024-11-23 13:36:21 +0900

    feat: 게시글 조회 기능 구현

--- src/main/java/com/TripLikeMovie/backend/domain/post/service/PostServiceImpl.java
+++ src/main/java/com/TripLikeMovie/backend/domain/post/service/PostServiceImpl.java
@@ -1,9 +1,11 @@
 package com.TripLikeMovie.backend.domain.post.service;
 
 import com.TripLikeMovie.backend.domain.member.domain.Member;
+import com.TripLikeMovie.backend.domain.movie.domain.Movie;
 import com.TripLikeMovie.backend.domain.post.domain.Post;
 import com.TripLikeMovie.backend.domain.post.domain.repository.PostRepository;
 import com.TripLikeMovie.backend.domain.post.presentation.dto.request.WritePostRequest;
+import com.TripLikeMovie.backend.global.error.exception.post.PostNotFoundException;
 import com.TripLikeMovie.backend.global.utils.image.ImageUtils;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,14 +22,22 @@ public class PostServiceImpl implements PostService {
     private final ImageUtils imageUtils;
 
     @Override
-    public void writePost(Member member, WritePostRequest postData, List<MultipartFile> images) {
+    public void writePost(Member member, Movie movie, WritePostRequest postData,
+        List<MultipartFile> images) {
 
-        Post post = new Post(member, postData.getContent(), postData.getLocationName(), postData.getLocationAddress());
+        Post post = new Post(member, movie, postData.getContent(), postData.getLocationName(),
+            postData.getLocationAddress());
 
         for (MultipartFile image : images) {
             String filePath = imageUtils.saveImage(image, "posts/");
             post.addFilePath(filePath);
         }
         postRepository.save(post);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Post findById(Integer postId) {
+        return postRepository.findById(postId).orElseThrow(() -> PostNotFoundException.EXCEPTION);
     }
 }
