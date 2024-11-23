db875951aae40436d47d341e2877ea72b975e9c3
jongchan0109 <jongchan1009@naver.com>
2024-11-23 16:13:37 +0900

    feat: 댓글 생성자 구현

--- src/main/java/com/TripLikeMovie/backend/domain/comment/domain/Comment.java
+++ src/main/java/com/TripLikeMovie/backend/domain/comment/domain/Comment.java
@@ -1,6 +1,8 @@
 package com.TripLikeMovie.backend.domain.comment.domain;
 
+import com.TripLikeMovie.backend.domain.comment.domain.vo.CommentVo;
 import com.TripLikeMovie.backend.domain.member.domain.Member;
+import com.TripLikeMovie.backend.domain.member.domain.vo.MemberInfoVo;
 import com.TripLikeMovie.backend.domain.post.domain.Post;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,9 +11,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "comments")
+@NoArgsConstructor
 public class Comment {
 
     @Id
@@ -27,5 +31,25 @@ public class Comment {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    public Comment(Member member, Post post, String content) {
+        this.member = member;
+        this.post = post;
+        this.content = content;
+
+        this.post.getComments().add(this);
+    }
+
+    public CommentVo getCommentInfo() {
+        MemberInfoVo memberInfo = member.getMemberInfo();
+
+        return CommentVo.builder()
+            .content(content)
+            .id(id)
+            .authorId(memberInfo.getId())
+            .authorNickname(memberInfo.getNickname())
+            .authorImageUrl(memberInfo.getImageUrl())
+            .build();
+    }
 
 }
