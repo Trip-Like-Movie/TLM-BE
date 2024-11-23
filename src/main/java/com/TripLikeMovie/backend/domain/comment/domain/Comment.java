69e1549c158a0e1126b9e83c059b7e44b22d81a5
jongchan0109 <jongchan1009@naver.com>
2024-11-23 15:51:21 +0900

    feat: 댓글 도메인 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/comment/domain/Comment.java
@@ -0,0 +1,31 @@
+package com.TripLikeMovie.backend.domain.comment.domain;
+
+import com.TripLikeMovie.backend.domain.member.domain.Member;
+import com.TripLikeMovie.backend.domain.post.domain.Post;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+}
