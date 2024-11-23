e3b3949aa507e6858e2d6e9514005f7fa6be092a
jongchan0109 <jongchan1009@naver.com>
2024-11-23 13:37:44 +0900

    feat: 게시글 정보 반환 기능 구현

--- src/main/java/com/TripLikeMovie/backend/domain/post/domain/Post.java
+++ src/main/java/com/TripLikeMovie/backend/domain/post/domain/Post.java
@@ -1,6 +1,9 @@
 package com.TripLikeMovie.backend.domain.post.domain;
 
 import com.TripLikeMovie.backend.domain.member.domain.Member;
+import com.TripLikeMovie.backend.domain.movie.domain.Movie;
+import com.TripLikeMovie.backend.domain.post.domain.vo.PostInfoVo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +14,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,23 +32,51 @@ public class Post {
     private String content;
 
     @ManyToOne
+    @JsonIgnore
     private Member member;
 
     @ElementCollection
-    private final List<String> filePaths = new ArrayList<>();
+    private final List<String> imageUrls = new ArrayList<>();
 
     private String locationAddress;
 
     private String locationName;
 
-    public Post(Member member, String content, String locationName, String locationAddress) {
+    @ManyToOne
+    @JsonIgnore
+    private Movie movie;
+
+    public Post(Member member, Movie movie, String content, String locationName,
+        String locationAddress) {
         this.content = content;
         this.member = member;
+        this.movie = movie;
         this.locationName = locationName;
         this.locationAddress = locationAddress;
+
+        if (movie != null) {
+            movie.getPosts().add(this);
+        }
+        if (member != null) {
+            member.getPosts().add(this);
+        }
     }
 
-    public void addFilePath(String filePath) {
-        filePaths.add(filePath);
+    public void addFilePath(String imageUrl) {
+        imageUrls.add(imageUrl);
+    }
+
+    public PostInfoVo getPostInfoVo() {
+        return PostInfoVo.builder()
+            .content(content)
+            .id(id)
+            .locationAddress(locationAddress)
+            .locationName(locationName)
+            .imageUrls(imageUrls.stream()
+                .map(imageUrl -> imageUrl.substring(imageUrl.lastIndexOf("TLM-BE/") + 7))  // 각 imageUrl에 substring 처리
+                .collect(Collectors.toList()))
+            .movieInfo(movie.getMovieInfo())
+            .memberInfo(member.getMemberInfo())
+            .build();
     }
 }
