d9591738fc2c04a7e8027411690c3b76705bdc62
jongchan0109 <jongchan1009@naver.com>
2024-11-23 15:45:27 +0900

    feat: 사용자 조회 API 구현

--- src/main/java/com/TripLikeMovie/backend/domain/member/presentation/MemberController.java
+++ src/main/java/com/TripLikeMovie/backend/domain/member/presentation/MemberController.java
@@ -20,6 +20,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -77,6 +78,11 @@ public class MemberController {
     @GetMapping
     public MemberInfoVo getMemberInfo() {
         return memberUtils.getMemberFromSecurityContext().getMemberInfo();
+    }
+
+    @GetMapping("/{memberId}")
+    public MemberInfoVo getMemberInfo(@PathVariable("memberId") Integer memberId) {
+        return memberService.findById(memberId).getMemberInfo();
     }
 
     @PostMapping("/change-password/verify-email")
