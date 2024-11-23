c0d3098b75667923a4de359faaca88fddf2e0868
jongchan0109 <jongchan1009@naver.com>
2024-11-23 16:14:20 +0900

    feat: 댓글 생성 API 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/comment/presentation/CommentController.java
@@ -0,0 +1,35 @@
+package com.TripLikeMovie.backend.domain.comment.presentation;
+
+import com.TripLikeMovie.backend.domain.comment.presentation.dto.request.WriteCommentRequest;
+import com.TripLikeMovie.backend.domain.comment.service.CommentService;
+import com.TripLikeMovie.backend.domain.member.domain.Member;
+import com.TripLikeMovie.backend.domain.post.domain.Post;
+import com.TripLikeMovie.backend.domain.post.service.PostService;
+import com.TripLikeMovie.backend.global.utils.member.MemberUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/posts/{postId}/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+    private final MemberUtils memberUtils;
+    private final PostService postService;
+
+    @PostMapping
+    public void createComment(@PathVariable Integer postId,
+        @RequestBody WriteCommentRequest writeCommentRequest) {
+
+        Member member = memberUtils.getMemberFromSecurityContext();
+        Post post = postService.findById(postId);
+        commentService.writeComment(member,post, writeCommentRequest);
+
+    }
+
+}
