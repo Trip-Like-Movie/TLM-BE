dfac94e1ccc37324e79dc3ac7a9f078059777577
jongchan0109 <jongchan1009@naver.com>
2024-11-23 19:34:23 +0900

    feat: 신고 요청 DTO 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/post/presentation/dto/request/ReportRequest.java
@@ -0,0 +1,10 @@
+package com.TripLikeMovie.backend.domain.post.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReportRequest {
+    String reason;
+}
