5c29622c535ff56db5b59527b8d1cfd4806c394f
jongchan0109 <jongchan1009@naver.com>
2024-11-23 17:34:51 +0900

    feat: comment에 getter 추가

--- src/main/java/com/TripLikeMovie/backend/domain/comment/domain/Comment.java
+++ src/main/java/com/TripLikeMovie/backend/domain/comment/domain/Comment.java
@@ -11,8 +11,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @Table(name = "comments")
 @NoArgsConstructor
