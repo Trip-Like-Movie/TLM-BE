198af07525a02a62ab16ccd85c4f2246ecc9bf1f
jongchan0109 <jongchan1009@naver.com>
2024-11-23 14:57:41 +0900

    feat: 게시글 반환 DTO 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/post/presentation/dto/response/AllPostResponse.java
@@ -0,0 +1,16 @@
+package com.TripLikeMovie.backend.domain.post.presentation.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AllPostResponse {
+    private Integer id;
+    private String firstImageUrl;
+
+    private String movieTitle;
+    private Integer MemberId;
+    private String memberNickname;
+    private String memberImageUrl;
+}
