d2cd9eb4961cdb3ad76a00f18e1c4f88f1c8104a
jongchan0109 <jongchan1009@naver.com>
2024-11-23 13:40:55 +0900

    feat: 사용자 조회시 영화 봔한 기능 구현

--- src/main/java/com/TripLikeMovie/backend/domain/member/domain/Member.java
+++ src/main/java/com/TripLikeMovie/backend/domain/member/domain/Member.java
@@ -1,6 +1,8 @@
 package com.TripLikeMovie.backend.domain.member.domain;
 
 import com.TripLikeMovie.backend.domain.member.domain.vo.MemberInfoVo;
+import com.TripLikeMovie.backend.domain.movie.domain.vo.MovieInfoVo;
+import com.TripLikeMovie.backend.domain.post.domain.Post;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,7 +10,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,19 +47,39 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    private String profileImageUrl;
+    private String imageUrl;
+
+    @OneToMany
+    private List<Post> posts = new ArrayList<>();
 
     public MemberInfoVo getMemberInfo() {
+
+        String profileImageUrl = (imageUrl != null)
+            ? imageUrl.substring(imageUrl.lastIndexOf("TLM-BE/") + 7)
+            : null;  // 기본 이미지를 사용하지 않고 null을 반환
+
+        // 중복 없는 영화 추출
+        Set<MovieInfoVo> uniqueMovies = posts.stream()
+            .map(Post::getMovie)
+            .distinct()
+            .map(movie -> MovieInfoVo.builder()
+                .id(movie.getId())
+                .title(movie.getTitle())
+                .build())
+            .collect(Collectors.toSet());
+
+        // MemberInfoVo 생성
         return MemberInfoVo.builder()
             .id(id)
             .email(email)
             .nickname(nickname)
-            .profileImageUrl(profileImageUrl)
+            .imageUrl(profileImageUrl)
+            .movies(uniqueMovies) // 추가된 영화 목록
             .build();
     }
 
-    public void updateProfileImage(String profileImageUrl) {
-        this.profileImageUrl = profileImageUrl;
+    public void updateProfileImage(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     public void changePassword(String encodedPassword) {
--- src/main/java/com/TripLikeMovie/backend/domain/member/domain/vo/MemberInfoVo.java
+++ src/main/java/com/TripLikeMovie/backend/domain/member/domain/vo/MemberInfoVo.java
@@ -1,5 +1,7 @@
 package com.TripLikeMovie.backend.domain.member.domain.vo;
 
+import com.TripLikeMovie.backend.domain.movie.domain.vo.MovieInfoVo;
+import java.util.Set;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,5 +12,7 @@ public class MemberInfoVo {
     private final Integer id;
     private final String email;
     private final String nickname;
-    private final String profileImageUrl;
+    private final String imageUrl;
+    private Set<MovieInfoVo> movies; // 사용자와 관련된 영화 정보
+
 }
