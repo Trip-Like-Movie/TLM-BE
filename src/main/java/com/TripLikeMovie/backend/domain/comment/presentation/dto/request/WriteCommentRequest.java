ece181631faa34c9b96ffac240aefa23d350074c
jongchan0109 <jongchan1009@naver.com>
2024-11-23 16:15:35 +0900

    feat: 게시글 요청 DTO 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/comment/presentation/dto/request/WriteCommentRequest.java
@@ -0,0 +1,10 @@
+package com.TripLikeMovie.backend.domain.comment.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WriteCommentRequest {
+    private String content;
+}
