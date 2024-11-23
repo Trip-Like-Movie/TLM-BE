70e9b0804fe86ca4c3af011dae70744ec819e9fc
jongchan0109 <jongchan1009@naver.com>
2024-11-23 17:38:36 +0900

    feat: 좋아요 도메인 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/like/domain/Like.java
@@ -0,0 +1,38 @@
+package com.TripLikeMovie.backend.domain.like.domain;
+
+import com.TripLikeMovie.backend.domain.member.domain.Member;
+import com.TripLikeMovie.backend.domain.post.domain.Post;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "likes")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public Like(Member member, Post post) {
+        this.member = member;
+        this.post = post;
+    }
+}
