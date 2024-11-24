753a7c1bbd5fc2f4f5920e5e337bd2fdd98df88b
jongchan0109 <jongchan1009@naver.com>
2024-11-24 22:07:36 +0900

    feat: 사용자 gpt 대화 객체 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/openai/domain/ConversationHistory.java
@@ -0,0 +1,35 @@
+package com.TripLikeMovie.backend.domain.openai.domain;
+
+import com.TripLikeMovie.backend.domain.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "conversation_histories")
+@NoArgsConstructor
+public class ConversationHistory {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Lob
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    public ConversationHistory(Member member, String message) {
+        this.member = member;
+        this.message = message;
+    }
+}
