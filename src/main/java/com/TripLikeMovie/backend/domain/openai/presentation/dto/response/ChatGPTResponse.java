6a0236647fb9dab6f5d88e33396d685f12df7b66
jongchan0109 <jongchan1009@naver.com>
2024-11-24 22:05:07 +0900

    feat: GPT 가 응답한 데이터 구현

---
+++ src/main/java/com/TripLikeMovie/backend/domain/openai/presentation/dto/response/ChatGPTResponse.java
@@ -0,0 +1,22 @@
+package com.TripLikeMovie.backend.domain.openai.presentation.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatGPTResponse {
+    private List<Choice> choices;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Choice {
+        private int index;
+        private Message message;
+
+    }
+}
