baf69f54d28e4c24cc7a7af9df83ff4b7c220523
jongchan0109 <jongchan1009@naver.com>
2024-11-24 14:18:04 +0900

    feat: 영화 랭킹 조회 구현

--- src/main/java/com/TripLikeMovie/backend/domain/movie/domain/repository/MovieRepository.java
+++ src/main/java/com/TripLikeMovie/backend/domain/movie/domain/repository/MovieRepository.java
@@ -4,8 +4,12 @@ import com.TripLikeMovie.backend.domain.movie.domain.Movie;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
     Optional<Movie> findByTitle(String title);
     List<Movie> findByTitleContainingIgnoreCase(String title);
+
+    @Query("SELECT m FROM Movie m LEFT JOIN m.posts p GROUP BY m.id ORDER BY COUNT(p) DESC LIMIT 3")
+    List<Movie> findTop3Movies();
 }
