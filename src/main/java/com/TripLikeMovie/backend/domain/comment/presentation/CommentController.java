ab76e12db901bea20012b354c69f10ba4e669a8d
jongchan0109 <jongchan1009@naver.com>
2024-11-24 22:05:29 +0900

    fix: API 수정

--- src/main/java/com/TripLikeMovie/backend/domain/comment/presentation/CommentController.java
+++ src/main/java/com/TripLikeMovie/backend/domain/comment/presentation/CommentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/posts/{postId}/comments")
+@RequestMapping("api/v1/post/{postId}/comments")
 public class CommentController {
 
     private final CommentService commentService;
