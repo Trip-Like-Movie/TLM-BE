8e6d0b9f74d274b82ef5d466b09965e218cd083b
jongchan0109 <jongchan1009@naver.com>
2024-11-25 17:43:46 +0900

    feat: AI 영화 추천 기능 구현

--- src/main/java/com/TripLikeMovie/backend/domain/openai/presentation/ChatGPTController.java
+++ src/main/java/com/TripLikeMovie/backend/domain/openai/presentation/ChatGPTController.java
@@ -1,11 +1,15 @@
 package com.TripLikeMovie.backend.domain.openai.presentation;
 
 import com.TripLikeMovie.backend.domain.member.domain.Member;
+import com.TripLikeMovie.backend.domain.member.domain.vo.MemberInfoVo;
+import com.TripLikeMovie.backend.domain.movie.domain.vo.MovieInfoVo;
 import com.TripLikeMovie.backend.domain.openai.presentation.dto.response.MessageResponse;
 import com.TripLikeMovie.backend.domain.openai.service.ChatGPTService;
 import com.TripLikeMovie.backend.domain.openai.presentation.dto.request.ChatGPTMessageRequest;
 import com.TripLikeMovie.backend.global.utils.member.MemberUtils;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +33,8 @@ public class ChatGPTController {
         String prompt;
         if ("영화".equals(chatGPTMessageRequest.getType())) {
             prompt = getMoviePrompt(chatGPTMessageRequest.getPrompt());
+        } else if ("추천".equals(chatGPTMessageRequest.getType())) {
+            prompt = getRecommendPrompt(member);
         } else {
             prompt = chatGPTMessageRequest.getPrompt();
         }
@@ -43,7 +49,8 @@ public class ChatGPTController {
         chatGPTService.saveConversation(member, "ChatGPT: " + result);
 
         log.info("ChatGPTResponse: {}", result);
-        result = result.replaceAll("(?i)^(ChatGPT:|Assistant:)", "").trim();  // "ChatGPT:" 또는 "Assistant:" 제거
+        result = result.replaceAll("(?i)^(ChatGPT:|Assistant:)", "")
+            .trim();  // "ChatGPT:" 또는 "Assistant:" 제거
 
         MessageResponse messageResponse = new MessageResponse();
         messageResponse.setMessage(result);
@@ -54,6 +61,33 @@ public class ChatGPTController {
 
     // 영화 프롬프트 생성
     private String getMoviePrompt(String movieName) {
-        return "영화 '" + movieName + "'의 촬영지 3곳과 그에 해당하는 관광지명과 주소를 알려주세요. 가능하다면, 그 장소에서 어떠한 장면을 찍었는지도 알려주세요.";
+        return "영화 '" + movieName
+            + "'의 촬영지 3곳과 그에 해당하는 관광지명과 주소를 알려주세요. 가능하다면, 그 장소에서 어떠한 장면을 찍었는지도 알려주세요.";
     }
+
+    private String getRecommendPrompt(Member member) {
+        List<MovieInfoVo> movies = member.getMemberInfo().getMovies();
+
+        List<String> movieTitles = new ArrayList<>();
+
+        for (MovieInfoVo movieInfoVo : movies) {
+            movieTitles.add(movieInfoVo.getTitle());
+        }
+        if (movieTitles.isEmpty()) {
+            return "대한민국에서 가장 유명한 영화의 촬영지 3곳과 그에 해당하는 관광지명과 주소를 알려주세요. 가능하다면, 그 장소에서 어떠한 장면을 찍었는지도 알려주세요.";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String movieTitle : movieTitles) {
+            sb.append(movieTitle).append(",");
+        }
+
+        return "제가 본 영화와 유사한 영화와 그 영화의 촬영지 3곳과 그에 해당하는 관광지명과 주소를 알려주세요. 가능하다면, 그 장소에서 어떠한 장면을 찍었는지도 알려주세요."
+            + "제가 본 영화는 " + sb + "가 있습니다."
+            + "대답을 할 때에는 사용자 영화 선호도 기반으로 추천해드립니다. 라고 말해줘"
+            + "대답을 할 때에는 내가 언급했다는 말을 하지 말아줘" ;
+
+    }
+
 }
--- src/main/java/com/TripLikeMovie/backend/domain/openai/presentation/dto/response/MessageResponse.java
+++ src/main/java/com/TripLikeMovie/backend/domain/openai/presentation/dto/response/MessageResponse.java
@@ -1,5 +1,10 @@
 package com.TripLikeMovie.backend.domain.openai.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +18,10 @@ import lombok.Setter;
 public class MessageResponse {
 
     private String message;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime timestamp;
 
 }
